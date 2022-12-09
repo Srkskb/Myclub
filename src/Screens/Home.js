07df9105d602d25d8aad9b53c42801bf11ef04ae
evaanand <evaanand@gmail.com>
2022-12-09 22:52:32 +0530

    Revert "c"
    This reverts commit 05bcdedad43147fb7c16dc10a1fd00ba37dfca38.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,5 +1,5 @@
-import { View, Text, StatusBar, Image, StyleSheet,ScrollView ,FlatList} from "react-native";
-import React, { useState } from "react";
+import { View, Text, StatusBar, Image, StyleSheet,ScrollView ,FlatList,Dimensions} from "react-native";
+import React from "react";
 import color from "../theme/color";
 import Header from "../component/Header.js";
 import SearchBox from "../component/SearchBox.js";
@@ -11,10 +11,11 @@ import {
 } from "react-native-responsive-screen";
 import Heading from "../component/Heading";
 import MyBagClubCard from "../component/MyBagClubCard";
-import Carousel from 'react-native-snap-carousel';
-
+import Carousel from 'react-native-reanimated-carousel';
+const width = Dimensions.get('window').width;
 export default function Home() {
-  const[Banner,setBanner]=useState([])
+  const [banner, setBanner] = React.useState([])
+  const getBanner=()=>{
   var data = qs.stringify({
     'banner': '1' 
   });
@@ -30,9 +31,31 @@ export default function Home() {
   };
   
   axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
+  .then((response) => {
+          if (response.data.success == 1) {
+            setBanner(response.data.data)
+          }
   })
+}
+React.useEffect(() => {
+  getBanner()
+}, [])
+   const _renderItem=({item,index})=>{
+        return (
+          <View style={{
+              borderRadius: 5,
+              height: hp(26),
+              marginLeft: 12,
+              marginRight: 12, }}>
+          <Image
+            style={{ height: hp(26), width:'100%' }}
+            source={{uri:item.Banner}}
+            resizeMode={'contain'}
+          />
+          </View>
+
+        )
+    }
   return (
     <ScrollView>
       <View>
@@ -41,10 +64,16 @@ export default function Home() {
 
         <SearchBox />
         <View style={{ width: "100%", alignSelf: "center", paddingTop: 15 }}>
-          <Image
-            style={{ height: hp(26), width: hp(54) }}
-            source={require("../images/ScrollImage.png")}
-          />
+          <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={banner}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={_renderItem}
+                />
         </View>
         <Text style={styles.text}>All Categories</Text>
         <View>
